c204ca0c3c949a9c9f7c8b7c94976e713f29c47b
kasiincognito <razafindrakotokasi@gmail.com>
2025-04-03 19:56:26 +0300

    color palette and mechanic changes

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,15 @@ const gender = document.getElementById('gender')
 var welcome = document.getElementById('welcome')
 var welcome2 = document.getElementById('welcome2')
 const start = document.getElementById("start")
+const choice1 = document.getElementById("choice1")
+const choice2 = document.getElementById("choice2")
+const choice3 = document.getElementById("choice3")
+const choice4 = document.getElementById("choice4")
+
+choice1.style.display = "none"
+choice2.style.display = "none"
+choice3.style.display = "none"
+choice4.style.display = "none"
 
 gender.style.display = "none"
 start.style.display = "none"
@@ -16,17 +25,6 @@ setTimeout(function(){
     logo.style.top = (10 + "px")
     loading.style.display = "none"
     gender.style.display = "block"
-
-    function animate(insert, out){ //une fonction que j'ai crée pour avoir un effet de ligne de commande sur les textes
-        var outList = out.textContent.split("")
-        var insertList = insert.split("")
-        if(outList.length < insertList.length){
-            out.textContent += insertList[outList.length]
-            setTimeout(function (){
-                animate(insert, out)
-            }, 30)
-        }
-    }
     
     setTimeout(function(){
         animate(content, welcome) //Je ne sais pas optimiser cette partie, aidez Kasi
@@ -34,14 +32,26 @@ setTimeout(function(){
             animate(content2, welcome2)
             setTimeout(function(){
                 start.style.display = "block"
-            }, 2000)
+            }, 1000)
         }, 5000)
     }, 600)
     
-}, 3000)
+}, 5000)
+
 
 gender.style.height = (window.innerHeight - 51 + "px")
 loading.style.top = ((window.innerHeight / 2) - 51 + "px")
 logo.style.top = ((window.innerHeight / 2) - 180 + "px")
+
+function animate(insert, out){ //une fonction que j'ai crée pour avoir un effet de ligne de commande sur les textes
+    var outList = out.textContent.split("")
+    var insertList = insert.split("")
+    if(outList.length < insertList.length){
+        out.textContent += insertList[outList.length]
+        setTimeout(function (){
+            animate(insert, out)
+        }, 30)
+    }
+}
 
 
