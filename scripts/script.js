8db997939d2de1a27e14996e26e02103362fca2d
kasiincognito <razafindrakotokasi@gmail.com>
2025-04-16 19:38:31 +0300

    building system

--- scripts/script.js
+++ scripts/script.js
@@ -10,12 +10,14 @@ const choice3 = document.getElementById("choice3")
 const choice4 = document.getElementById("choice4")
 const ageinput = document.getElementById("age")
 const first = document.getElementById('first')
+const result = document.getElementById("result")
 var numstate = 0
 const buttonnum = 0
 var character = [0, 0, 0]
 var nom = ""
 start.textContent = "Get Started"
 
+result.style.display = "none"
 choice1.style.display = "none"
 choice2.style.display = "none"
 choice3.style.display = "none"
@@ -57,7 +59,7 @@ setTimeout(function(){
             }, 100)
         }, 100)
     }, 100)
-}, 5000)
+}, 100)
 
 
 gender.style.height = (window.innerHeight - 51 + "px")
@@ -113,13 +115,14 @@ function stages(state, num){
         choice1.style.display = "block"
         choice2.style.display = "block"
         choice1.textContent = "Female"
-        choice2.textContent = "LGBTQI+"
+        choice2.textContent = "Non-binary"
         age = parseInt(ageinput.value)
     }else if(state == 3){
         start.textContent = "Hot"
         choice1.textContent = "Middle"
         choice2.textContent = "Skinny"
         if(num == 1){
+            character[0] = 0
             animate(content[5], welcome)
         }else if(num == 2){
             character[0] = 1
@@ -131,23 +134,25 @@ function stages(state, num){
             choice1.style.display = "none"
             choice2.style.display = "none"
         }
+        alert(character)
     }else if(state == 4){
         if(character[0] == 0){
             animate(content[7], welcome)
-        }else{
+        }else if(character[0] == 1){
             animate(content[8], welcome)
         }
         start.textContent = "Savage"
         choice1.textContent = "Calm"
         choice2.textContent = "Dirty"
         if(num == 1){
-            character[1] == 0
+            character[1] = 0
         }else if(num == 2){
-            character[1] == 1
+            character[1] = 1
+            
+        }else if(num == 3){
+            character[1] = 2
         }
-        else if(num == 3){
-            character[1] == 2
-        }
+        alert(character)
     }else if(state == 5){
         ageinput.value = ""
         ageinput.style.display = "block"
@@ -168,6 +173,7 @@ function stages(state, num){
         else if(num == 3){
             character[2] = 2
         }
+        alert(character)
     }else if(state == 6){
         gender.style.display = "none"
         logo.style.display = "block"
@@ -176,6 +182,7 @@ function stages(state, num){
             gender.style.display = "block"
             ageinput.style.display = "none"
             start.style.display = "none"
+            animate(nom, welcome)
             gen(character)
         }, 3000)
         if(character[0] == 0){
@@ -188,43 +195,48 @@ function stages(state, num){
 
 }
 
-function gen(character){
-    if(character = [0, 0, 0]){         // 1
-
-    }else if(character = [0, 0, 1]){// 2
-        
-    }else if(character = [0, 0, 2]){// 3
-        
-    }else if(character = [0, 1, 0]){// 4
-        
-    }else if(character = [0, 1, 1]){// 5
-        
-    }else if(character = [0, 1, 2]){// 6
-        
-    }else if(character = [0, 2, 0] ){// 7
-        
-    }else if(character = [0, 2, 1] ){// 8
-        
-    }else if(character = [0, 2, 2]){// 9
-        
-    }else if(character = [1, 0, 0]){// 10
-        
-    }else if(character = [1, 0, 1]){// 11
-        
-    }else if(character = [1, 0, 2]){// 12
-        
-    }else if(character = [1, 1, 0]){// 13
-        
-    }else if(character = [1, 1, 1]){// 14
-        
-    }else if(character = [1, 1, 2]){// 15
-        
-    }else if(character = [1, 2, 0]){// 16
-        
-    }else if(character = [1, 2, 1]){// 17
-        
-    }else if(character = [1, 2, 2]){// 18
-        
+function gen(res){
+    alert(res)
+    result.style.display = "block"
+    if(res[0] == 0 && res[1] == 0 && res[2] == 0){      // 1
+        result.src = "assets/100.jpg"
+    }else if(res[0] == 0 && res[1] == 0 && res[2] == 1){// 2
+        result.src = "assets/101.jpg"
+    }else if(res[0] == 0 && res[1] == 0 && res[2] == 2){// 3
+        result.src = "assets/102.jpg"
+    }else if(res[0] == 0 && res[1] == 1 && res[2] == 0){// 4
+        result.src = "assets/110.jpg"
+    }else if(res[0] == 0 && res[1] == 1 && res[2] == 1){// 5
+        result.src = "assets/111.jpg"
+    }else if(res[0] == 0 && res[1] == 1 && res[2] == 2){// 6
+        result.src = "assets/112.jpg"
+    }else if(res[0] == 0 && res[1] == 2 && res[2] == 0){// 7
+        result.src = "assets/120.jpg"
+    }else if(res[0] == 0 && res[1] == 2 && res[2] == 1){// 8
+        result.src = "assets/121.jpg"
+    }else if(res[0] == 0 && res[1] == 2 && res[2] == 2){// 9
+        result.src = "assets/122.jpg"
+    }else if(res[0] == 1 && res[1] == 0 && res[2] == 0){// 10
+        result.src = "assets/000.jpg"
+    }else if(res[0] == 1 && res[1] == 0 && res[2] == 1){// 11
+        result.src = "assets/001.jpg"
+    }else if(res[0] == 1 && res[1] == 0 && res[2] == 2){// 12
+        result.src = "assets/002.jpg"
+    }else if(res[0] == 1 && res[1] == 1 && res[2] == 0){// 13
+        result.src = "assets/010.jpg"
+    }else if(res[0] == 1 && res[1] == 1 && res[2] == 1){// 14
+        result.src = "assets/011.jpg"
+    }else if(res[0] == 1 && res[1] == 1 && res[2] == 1){// 15
+        result.src = "assets/012.jpg"
+    }else if(res[0] == 1 && res[1] == 2 && res[2] == 0){// 16
+        result.src = "assets/020.jpg"
+    }else if(res[0] == 1 && res[1] == 2 && res[2] == 1){// 17
+        result.src = "assets/021.jpg"
+    }else if(res[0] == 1 && res[1] == 2 && res[2] == 2){// 18
+        result.src = "assets/022.jpg"
+    }
+    else{
+        alert("error")
     }
 }
 
